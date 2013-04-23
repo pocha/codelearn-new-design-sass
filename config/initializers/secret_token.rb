54bdb06be5191add72db9e169bf6a921692b3754
pocha <pocha@stalkninja.com>
2013-04-23 13:42:48 +0530

    first commit with bootstrap page while sass variables can be overridden in application.css.scss

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodelearnNewDesignRails::Application.config.secret_token = 'e27b452f0592eabe0b759a9f6225728eda6100b7d8021b4f8d5c3102dc5e8d0a4d307e80208eb106de92371e93a00fb0ad8496bfca924f7fee04268db7931ce1'
